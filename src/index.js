53823dcd3745f86c70c8b16953bcd5e95b4a1c39
Alexandr Bet <alex.van.bet@gmail.com>
2019-09-25 20:34:52 +0300

    docs: add code section to index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function multiply(first, second) {
-  // your solution
+  let composition = 0;
+  composition = BigInt(first) * BigInt(second);
+  return(composition.toString());
 }
